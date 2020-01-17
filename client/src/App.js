19d53642cfa44b1c3f7a702d922a049c47568ec8
Greg Hayes <gthelight@gmail.com>
2020-01-17 10:56:21 -0600

    attempting to fetch API

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,40 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  state = {
+    id: 0,
+    name: "",
+    country: "",
+    searches: ""
+  };
+
+  componetDidMount() {
+    axios
+      .get("http://localhost:5000/api/players")
+      .then(res => {
+        console.log(res.data);
+        const player = res.data;
+        this.setState({
+          id: player.id,
+          name: player.name,
+          country: player.country,
+          searches: player.searches
+        });
+      })
+      .catch(err => {
+        console.log("Error loading API", err);
+      });
+  }
+  render() {
+    return (
+      <div className="App">
+        <h1>Women's World Cup Player Interest</h1>
+        <h3>By Google Search Interest</h3>
+      </div>
+    );
+  }
 }
 
 export default App;
